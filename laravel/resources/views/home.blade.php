35502ddaef61a21629d1b50b75d8c91e6a5b2a51
adnanqurais <free.acknowledge@gmail.com>
2016-10-04 11:50:08 +0700

    test

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -38,7 +38,7 @@
 <!--Mobile Category-->
 <div class="mobile-category container text-center">
 
-    <h3 class="featured-brand text-center" style="margin-bottom: 20px; letter-spacing:5px; font-weight:600;">SHOP CATEGORY</h3>
+    <h3 class="featured-brand text-center" style="margin-bottom: 20px; letter-spacing:5px; font-weight:600;">SHOP CATEGORY TEST LAGI</h3>
 
 
         @if ($category_view[0]->parent == '' || $category_view[0]->parent == '0' || $category_view[0]->parent == ' ')
